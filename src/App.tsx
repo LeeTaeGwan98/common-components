d5f9a74c412836b0a14734bb2b374c026031675a
LeeTaeGwan98 <itsydesign03@gmail.com>
2025-03-11 15:20:56 +0900

    chore: 공통컴포넌트 의존성 패키지 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,11 @@
-import { useEffect, useState } from "react";
-import { Toaster } from "@/components/ui/sonner";
-import CheckIcon from "@/assets/svg/check.svg";
-import { customToast } from "@/components/Atoms/Toast/Toast";
-import { BrowserRouter } from "react-router-dom";
-import Button from "@/components/Atoms/Button/Solid/Button";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-  SelectGroup,
-  SelectLabel,
-} from "@/components/ui/select";
-import SelectBox from "@/components/Molecules/SelectBox/SelectBox";
+import DatePicker from "@/components/Molecules/DatePicker/DatePicker";
+import { useState } from "react";
 
 function App() {
+  const [date, setDate] = useState<undefined | Date>();
   return (
     <div className="p-[40px]">
-      <SelectBox placeholder="placeholder">
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>asdf</SelectLabel>
-            <SelectItem value="asdf">asdf</SelectItem>
-            <SelectItem value="asdf1">asdf1</SelectItem>
-            <SelectItem value="asdf2">asdf2</SelectItem>
-            <SelectItem value="asdf3">asdf3</SelectItem>
-            <SelectItem value="asdf4">asdf4</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </SelectBox>
+      <DatePicker date={date} setDate={setDate} />
     </div>
   );
 }
