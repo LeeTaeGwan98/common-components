fd8b239661267023356e3ddb2217f8b4a37aebf7
양동규 <itsydev12@gmail.com>
2025-03-17 13:07:24 +0900

    feat: 관리자 페이지 router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import Layout from "@/components/base/layout";
 import Login from "@/pages/Login";
 import Main from "@/pages/Main";
 import UserList from "@/pages/User/UserList";
+import Terms from "@/pages/Admin/Terms";
+import CommonCode from "@/pages/Admin/CommonCode";
+import Account from "@/pages/Admin/Account";
+import TermsDetail from "@/pages/Admin/TermsDetail";
+import TermsRegistration from "@/pages/Admin/TermsRegistration";
 
 function App() {
   return (
@@ -12,6 +17,11 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/main" element={<Main />} />
           <Route path="/user-list" element={<UserList />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/common-code" element={<CommonCode />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/terms-detail" element={<TermsDetail />} />
+          <Route path="/terms-regisreation" element={<TermsRegistration />} />
           <Route path="/asdf" element={<div>asdf</div>} />
         </Routes>
       </Layout>
