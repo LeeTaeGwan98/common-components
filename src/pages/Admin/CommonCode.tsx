5f9ee1af5dcefc8640f3d3896dc8c63b8479d680
양동규 <itsydev12@gmail.com>
2025-03-17 13:06:00 +0900

    feat: 관리자 계정관리, 공통코드 관리 페이지 생성

---
+++ src/pages/Admin/CommonCode.tsx
@@ -0,0 +1,11 @@
+import BreadcrumbContainer from "@/components/BreadcrumbContainer";
+
+function CommonCode() {
+  return (
+    <BreadcrumbContainer breadcrumbNode={<>관리자 / 공통 코드 관리</>}>
+      sd
+    </BreadcrumbContainer>
+  );
+}
+
+export default CommonCode;
