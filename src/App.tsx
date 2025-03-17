c386ff985e2d8c4afa064ea240a76b7820d15bd6
우유들컹 <xpzm14@gmail.com>
2025-03-17 13:14:58 +0900

    Revert "Feat: 전자책 관리 App.tsc에 route 추가"
    This reverts commit c27530da17359998716d7d856f08e90546ab9217.

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,10 @@ import Layout from "@/components/base/layout";
 import Login from "@/pages/Login";
 import Main from "@/pages/Main";
 import UserList from "@/pages/User/UserList";
-import PublishList from "@/pages/Ebook/Publish/PublishList";
-import PublishListDetail from "@/pages/Ebook/Publish/PublishListDetail";
-import Cover from "@/pages/Ebook/Cover/Cover";
+import PublishList from "@/pages/Ebook/PublishList";
+import PublishListDetail from "@/pages/Ebook/PublishListDetail";
+import Cover from "@/pages/Ebook/Cover";
 import Charging from "@/pages/Ebook/Charging";
-import CoverDetail from "@/pages/Ebook/Cover/CoverDetail";
 
 function App() {
   return (
@@ -20,7 +19,6 @@ function App() {
           <Route path="/publish-list" element={<PublishList />} />
           <Route path="/publish-list-detail" element={<PublishListDetail />} />
           <Route path="/cover" element={<Cover />} />
-          <Route path="/cover-detail" element={<CoverDetail />} />
           <Route path="/charging" element={<Charging />} />
         </Routes>
       </Layout>
