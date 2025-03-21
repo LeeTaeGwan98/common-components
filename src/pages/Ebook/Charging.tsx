d99e756e0f53bc76d08e103d2e154d3e87ec0150
우유들컹 <xpzm14@gmail.com>
2025-03-21 19:12:57 +0900

    feat: 전자책 관리 -충전소 페이지 숫자만 입력되도록 수정

--- src/pages/Ebook/Charging.tsx
+++ src/pages/Ebook/Charging.tsx
@@ -2,46 +2,112 @@ import BreadcrumbContainer from "@/components/BreadcrumbContainer";
 import OutlinedButton from "@/components/common/Atoms/Button/Outlined/OutlinedButton";
 import TextField from "@/components/common/Molecules/TextField/TextField";
 import ContentWrapper from "@/components/ContentWrapper";
+import { Dispatch, SetStateAction, useState } from "react";
 
-interface TextListProps {
+interface DataListProps {
   number: number;
-  point: string;
-  amount: string;
+  point: number;
+  setPoint?: Dispatch<SetStateAction<number>>;
+  amount: number;
+  setAmount?: Dispatch<SetStateAction<number>>;
 }
 
-function TextList({ number, point, amount }: TextListProps) {
+function TextList({
+  number,
+  point,
+  setPoint,
+  amount,
+  setAmount,
+}: DataListProps) {
   return (
     <div className="flex justify-center *:flex-1 gap-[20px]">
       <TextField
         label={`${number}번 항목`}
-        value={point}
+        value={point.toLocaleString("kr")}
         subText="포인트"
         slot={{ subTextClassName: "text-label-alternative" }}
         isVisible={false}
+        onChange={(e) => {
+          // 숫자만 필터링
+          const numericValue = e.target.value.replace(/\D/g, "");
+
+          if (numericValue.length <= 6) {
+            setPoint && setPoint(Number(numericValue));
+          }
+        }}
       />
       <TextField
         label="&nbsp;"
-        value={amount}
+        value={amount.toLocaleString("kr")}
         isVisible={false}
         subText="금액"
         slot={{ subTextClassName: "text-label-alternative" }}
+        onChange={(e) => {
+          // 숫자만 필터링
+          const numericValue = e.target.value.replace(/\D/g, "");
+
+          if (numericValue.length <= 6) {
+            setAmount && setAmount(Number(numericValue));
+          }
+        }}
       />
     </div>
   );
 }
 
 function Charging() {
+  //가데이터
+  const [data, setData] = useState<DataListProps[]>([
+    {
+      number: 0,
+      point: 0,
+      amount: 0,
+    },
+    {
+      number: 1,
+      point: 0,
+      amount: 0,
+    },
+  ]);
+
+  // 포인트or금액상태를 업데이트하는 함수
+  const updateData = (
+    type: "point" | "amount",
+    index: number,
+    value: SetStateAction<number>
+  ) => {
+    setData((prevData) =>
+      prevData.map((item, i) =>
+        i === index
+          ? type === "point"
+            ? {
+                ...item,
+                point: typeof value === "function" ? value(item.point) : value,
+              }
+            : {
+                ...item,
+                amount:
+                  typeof value === "function" ? value(item.amount) : value,
+              }
+          : item
+      )
+    );
+  };
+
   return (
     <BreadcrumbContainer breadcrumbNode={<>전자책 관리 / 충전소 관리</>}>
       <ContentWrapper>
-        <TextList number={1} point={"1000"} amount={"1000"} />
-        <TextList number={2} point={"1000"} amount={"1000"} />
-        <TextList number={3} point={"1000"} amount={"1000"} />
-        <TextList number={4} point={"1000"} amount={"1000"} />
-        <TextList number={5} point={"1000"} amount={"1000"} />
-        <TextList number={6} point={"1000"} amount={"1000"} />
-        <TextList number={7} point={"1000"} amount={"1000"} />
-        <TextList number={8} point={"1000"} amount={"1000"} />
+        {data.map((item, index) => {
+          return (
+            <TextList
+              number={item.number}
+              point={item.point}
+              amount={item.amount}
+              setPoint={(value) => updateData("point", index, value)}
+              setAmount={(value) => updateData("amount", index, value)}
+            />
+          );
+        })}
         <div className="flex justify-end w-full">
           <OutlinedButton
             className="max-w-[180px] w-full"
