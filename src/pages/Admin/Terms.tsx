6b868774c90a2b09deb794f2405e552199680887
양동규 <itsydev12@gmail.com>
2025-03-21 09:49:12 +0900

    refactor: 페이지 라우터 방식 변경

--- src/pages/Admin/Terms.tsx
+++ src/pages/Admin/Terms.tsx
@@ -11,6 +11,7 @@ import {
 } from "@/components/common/Tables";
 import IconButton from "@/components/common/Atoms/Button/IconButton/IconButton";
 import ThreeDot from "@/assets/svg/common/threeDot.svg";
+import { TERMS_DETAIL } from "@/Constants/ServiceUrl";
 
 const data = [
   {
@@ -68,8 +69,7 @@ function Terms() {
                   <TableCell>{item.termsName}</TableCell>
                   <TableCell>{item.fileName}</TableCell>
                   <TableCell>
-                    <Link to={`/terms-detail`}>
-                      {/* <Link to={`/terms-detail/${index}`}></Link> 추후 이런식으로 수정?*/}
+                    <Link to={TERMS_DETAIL}>
                       <IconButton
                         icon={
                           <ThreeDot className="size-[24px] fill-label-alternative" />
