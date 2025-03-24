2333649f917413191fc45e9085a696118fdeb298
이태관 <cokid98@naver.com>
2025-03-24 15:59:19 +0900

    Refactor: CardRow slot className 추가

--- src/components/common/Molecules/CardRow/CardRow.tsx
+++ src/components/common/Molecules/CardRow/CardRow.tsx
@@ -1,32 +1,52 @@
 import { cn } from "@/lib/utils";
 import { cloneElement, ReactElement } from "react";
-
 interface CardRowDataType {
   title?: string;
   content?: string;
   shortcut?: ReactElement<{ className: string }>;
 }
-
 interface CardRowProps {
   data?: CardRowDataType;
   className?: string;
+  slot?: {
+    containerClassName?: string;
+    titleClassname?: string;
+    contentClassName?: string;
+    shortcutClassName?: string;
+  };
 }
-
-function CardRow({ data = {}, className }: CardRowProps) {
+function CardRow({ data = {}, slot = {} }: CardRowProps) {
   const { title, content, shortcut } = data;
   return (
-    <div className={cn("flex flex-col gap-[2px]", className)}>
-      <div className="text-label2-bold text-label-normal">{title}</div>
-      <div className="flex justify-between">
-        <span className="text-body2-normal-regular text-label-neutral">
-          {content}
-        </span>
+    <div className={cn("flex flex-col gap-[2px]", slot.containerClassName)}>
+      <div className="flex items-center justify-between">
+        <div>
+          <div
+            className={cn(
+              "text-label2-bold text-label-normal",
+              slot.titleClassname
+            )}
+          >
+            {title}
+          </div>
+          <span
+            className={cn(
+              "text-body2-normal-regular text-label-neutral",
+              slot.contentClassName
+            )}
+          >
+            {content}
+          </span>
+        </div>
         {shortcut && (
-          <span>{cloneElement(shortcut, { className: "size-[48px]" })}</span> // 기본 사이즈 48px
+          <span>
+            {cloneElement(shortcut, {
+              className: cn("size-[48px]", slot.shortcutClassName),
+            })}
+          </span> // 기본 사이즈 48px
         )}
       </div>
     </div>
   );
 }
-
 export default CardRow;
