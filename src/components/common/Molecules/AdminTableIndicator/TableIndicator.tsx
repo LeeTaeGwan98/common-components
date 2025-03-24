24e01c32a3dfc6928b21e28a39b13bcc1071e67a
우유들컹 <xpzm14@gmail.com>
2025-03-24 17:20:14 +0900

    no message

--- src/components/common/Molecules/AdminTableIndicator/TableIndicator.tsx
+++ src/components/common/Molecules/AdminTableIndicator/TableIndicator.tsx
@@ -1,15 +1,43 @@
 import IconButton from "@/components/common/Atoms/Button/IconButton/IconButton";
 import Search from "@/assets/svg/common/search.svg";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function TableIndicator() {
   const [inputPage, setInputPage] = useState<string>(""); //입력한 페이지
+
+  const spanRef = useRef<HTMLSpanElement | null>(null);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  //span의 너비에 맞춰 input 너비 늘려줌
+  useEffect(() => {
+    const spanElement = spanRef.current;
+    if (!spanElement) return;
+
+    const resizeObserver = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        if (spanRef.current && inputRef.current) {
+          const width = spanRef.current.getBoundingClientRect().width;
+          inputRef.current.style.width = `${width}px`;
+        }
+      }
+    });
+
+    resizeObserver.observe(spanElement);
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, []);
+
   return (
     <div className="flex gap-space-button-vertical">
-      <div className="w-min min-w-[137px] flex items-center gap-[8px] h-[40px] px-[10px] border-[1px] border-solid border-line-normal-normal rounded-radius-admin">
+      <div className="flex items-center gap-[8px] h-[40px] px-[10px] border-[1px] border-solid border-line-normal-normal rounded-radius-admin">
+        {/* span => input 너비를 측정하기 위한 투명 글자 */}
+        <span ref={spanRef} className="min-w-[40px] hide_span"></span>
         <input
+          ref={inputRef}
           className={
-            "w-[calc(100%-40px)] focus:outline-none text-body2-normal-medium text-label-neutral "
+            "focus:outline-none text-body2-normal-medium text-label-neutral "
           }
           value={inputPage}
           type={"text"}
