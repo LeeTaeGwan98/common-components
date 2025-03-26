b71a23323372b63f073a6fa8851ec34c7c3a32b3
우유들컹 <xpzm14@gmail.com>
2025-03-26 11:04:26 +0900

    Refactor(OutlinedButton.tsx): size별 둥글기 large(10px), medium(8px), small(6px) 3가지 추가

--- src/styles/borderRadius.ts
+++ src/styles/borderRadius.ts
@@ -1,6 +1,9 @@
 const borderRadius = {
   "radius-admin": "4px",
   "large-button": "10px",
+  "large-input": "12px",
+  "medium-button": "8px",
+  "small-button": "6px",
 };
 
 export default borderRadius;
