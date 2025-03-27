f382022a95ed8ba4609809db4ae332ad6ee79c07
이태관 <cokid98@naver.com>
2025-03-27 10:48:52 +0900

    Refactor(CardRow): 텍스트간 간격 추가

--- src/components/common/Molecules/CardRow/CardRow.tsx
+++ src/components/common/Molecules/CardRow/CardRow.tsx
@@ -1,10 +1,12 @@
 import { cn } from "@/lib/utils";
 import { cloneElement, ReactElement } from "react";
+
 interface CardRowDataType {
   title?: string;
   content?: string;
   shortcut?: ReactElement<{ className: string }>;
 }
+
 interface CardRowProps {
   data?: CardRowDataType;
   className?: string;
@@ -15,38 +17,39 @@ interface CardRowProps {
     shortcutClassName?: string;
   };
 }
+
 function CardRow({ data = {}, slot = {} }: CardRowProps) {
   const { title, content, shortcut } = data;
   return (
     <div className={cn("flex flex-col gap-[2px]", slot.containerClassName)}>
-      <div className="flex items-center justify-between">
-        <div>
-          <div
-            className={cn(
-              "text-label2-bold text-label-normal",
-              slot.titleClassname
-            )}
-          >
-            {title}
-          </div>
-          <span
-            className={cn(
-              "text-body2-normal-regular text-label-neutral",
-              slot.contentClassName
-            )}
-          >
-            {content}
-          </span>
-        </div>
-        {shortcut && (
-          <span>
-            {cloneElement(shortcut, {
-              className: cn("size-[48px]", slot.shortcutClassName),
-            })}
-          </span> // 기본 사이즈 48px
-        )}
+      <div className="flex flex-col gap-[6px]">
+        <span
+          className={cn(
+            "text-label2-bold text-label-normal",
+            slot.titleClassname
+          )}
+        >
+          {title}
+        </span>
+        <span
+          className={cn(
+            "text-body2-normal-regular text-label-neutral",
+            slot.contentClassName
+          )}
+        >
+          {content}
+        </span>
       </div>
+
+      {shortcut && (
+        <span>
+          {cloneElement(shortcut, {
+            className: cn("size-[48px]", slot.shortcutClassName),
+          })}
+        </span> // 기본 사이즈 48px
+      )}
     </div>
   );
 }
+
 export default CardRow;
