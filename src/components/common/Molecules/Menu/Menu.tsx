5a3f81b9e41fb48361d58775455463606f4ad75f
우유들컹 <xpzm14@gmail.com>
2025-04-30 13:46:03 +0900

    feat: 서브 메뉴 수정

--- src/components/common/Molecules/Menu/Menu.tsx
+++ src/components/common/Molecules/Menu/Menu.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import Label from "@/components/common/Atoms/Label/Label";
+import IconButton from "@/components/common/Atoms/Button/IconButton/IconButton";
 
 interface MenuProps extends Omit<React.HTMLAttributes<HTMLDivElement>, "slot"> {
   labelText?: string;
@@ -9,6 +10,7 @@ interface MenuProps extends Omit<React.HTMLAttributes<HTMLDivElement>, "slot"> {
   children: ReactNode;
   path?: string;
   icon?: ReactNode;
+  onArrowIconClick?: (e: React.MouseEvent) => void;
   slot?: {
     containerClassName?: string;
     labelClassName?: string;
@@ -23,28 +25,70 @@ function Menu({
   children,
   className,
   icon,
+  onArrowIconClick,
   path,
   slot,
   ...props
 }: MenuProps) {
-  const interactiveTypeStyle =
-    "hover:bg-label-normal/light-hover focus:bg-label-normal/light-focus active:bg-label-normal/light-active";
+  const [isHovered, setIsHovered] = useState(false);
+  const [isIconHovered, setIsIconHovered] = useState(false); // ⭐️ 추가
+
+  const handleMouseEnter = () => {
+    if (!isIconHovered) setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const handleMouseMove = () => {
+    if (!isIconHovered && !isHovered) {
+      setIsHovered(true);
+    }
+  };
+
+  const handleIconMouseEnter = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setIsIconHovered(true);
+    setIsHovered(false);
+  };
+
+  const handleIconMouseLeave = () => {
+    setIsIconHovered(false);
+  };
+
+  const handleIconClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    onArrowIconClick && onArrowIconClick(e);
+  };
+
   return (
     <div
       className={cn(
-        "flex justify-between py-[13px] rounded-radius-admin",
-        interactiveTypeStyle,
+        "flex justify-between rounded-radius-admin",
+        isHovered && "bg-label-normal/light-hover",
         className
       )}
       {...props}
     >
-      <div className={cn("flex items-center")}>
-        {arrowIcon}
-        {icon}
-        <span className="ml-[4px]">{children}</span>
+      <div className="flex items-center flex-1">
+        <IconButton
+          icon={arrowIcon}
+          onMouseEnter={(e) => handleIconMouseEnter(e)}
+          onMouseLeave={handleIconMouseLeave}
+          onClick={handleIconClick}
+        />
+        <div
+          className="flex w-full items-center"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          onMouseMove={handleMouseMove}
+        >
+          {icon}
+          <span className="ml-[4px] w-full">{children}</span>
+        </div>
       </div>
 
-      <div className={cn("flex gap-[4px] items-center")}>
+      <div className="flex gap-[4px] items-center ">
         {labelText && (
           <Label
             variant="outlined"
@@ -54,14 +98,10 @@ function Menu({
             {labelText}
           </Label>
         )}
-        {rightIcon && rightIcon}
+        {rightIcon}
       </div>
     </div>
   );
 }
 
 export default Menu;
-
-/**
- * Todo: asChild 패턴으로 버튼태그로 구현
- */
